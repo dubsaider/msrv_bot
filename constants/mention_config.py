f5cae354f968b6213ef9e2c68e998f623edd5438
Kostenkov Vladislav <kostenkov.va@dvfu.ru>
2025-06-26 15:03:35 +1000

    update phrase

--- constants/mention_config.py
+++ constants/mention_config.py
@@ -3,6 +3,9 @@ import re
 ALL_MENTION_PATTERNS = [
     ("Паша", re.compile(r'\bпаш(а|ей|у|е|и|енька|ка)?\b', re.IGNORECASE)),
     ("Pasha", re.compile(r'\bpasha\b', re.IGNORECASE)),
+
+    ("Павел", re.compile(r'\b(павел(а|у|ом|е)?|павлик(а|у|ом|е)?|павлуша|пашенька)\b', re.IGNORECASE)),
+    ("Pavel", re.compile(r'\bpavel\b', re.IGNORECASE)),
     
     ("Захар", re.compile(r'\bзахар(а|у|ом|е|чик|ка)?\b', re.IGNORECASE)),
     ("Zakhar", re.compile(r'\bzahar\b', re.IGNORECASE)),
