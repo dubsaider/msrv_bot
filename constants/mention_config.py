bbdcf87219614a1608f5f3fe6646e8f1fe66545a
Kostenkov Vladislav <kostenkov.va@dvfu.ru>
2025-06-26 13:55:54 +1000

    fix

--- constants/mention_config.py
+++ constants/mention_config.py
@@ -4,7 +4,7 @@ ALL_MENTION_PATTERNS = [
     ("Паша", re.compile(r'\bпаш(а|ей|у|е|и|енька|ка)?\b', re.IGNORECASE)),
     ("Pasha", re.compile(r'\bpasha\b', re.IGNORECASE)),
     
-    ("ЗАхар", re.compile(r'\bзахар(а|у|ом|е|чик|ка)?\b', re.IGNORECASE)),
+    ("Захар", re.compile(r'\bзахар(а|у|ом|е|чик|ка)?\b', re.IGNORECASE)),
     ("Zakhar", re.compile(r'\bzahar\b', re.IGNORECASE)),
     
     ("Дед", re.compile(r'\bдед(а|у|ом|е|ок|ушка)?\b', re.IGNORECASE)),
